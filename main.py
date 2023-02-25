cff0cd716693525a62ca508711465e68992a9d0a
Fayzer789 <fayzer.coding@gmail.com>
2023-02-25 04:38:57 +0100

    escape titanic v1.2 not finished

--- main.py
+++ main.py
@@ -1,34 +1,31 @@
-mc_name = input("What's your name?")
-print("Welcome", mc_name, "you are currently in a boat that just hit an iceberg, your goal is to get out of here in LIFE ")
+# Author : Fayzer789
 
-answer = input(
-    "You can only turn left or right, Which way would you go? Be careful the wrong path might lead you to a painful death" ).lower()
+def intro():
+    print("zzzzzzzzzzzzzzzzz")
+    print("*alarm clock noise* DRINGGGGGGG")
+    print("WHatttt can't you let me sleep just a little bit man")
+    print("*you disable the alarm*")
+    print("*You scratch your eyes with your hands for a bit and get up*")
+    print("Fuck off I was getting extra good sleep if only that damn thing hadn’t woken me up")
+    print("*You look at the window")
+    print("Oh that's weird what is it already the night?")
+    print("Wait")
+    print("...")
+    print("Don't tell me")
+    print("............")
+    print("IT'S ALREADY 9 PM")
+    print("I’M 20 MINUTES LATE FOR THE BIG CRUISE MEAL")
+    print("*you dress so fast that you have completely forgotten your tie*")
+    print("I hope I won't be in late")
+    print("*you're running as fast you can to not get too late*")
+    print("...But")
+    print("suddenly a loud noise is heard..")
+    print("BAMMMMMMMMMM")
+    print("It was a sudden collision that made the whole ship vibrate")
+    print("Lights go off and a piercing red alarm starts to ring in your ears")
+    print("        *****************************************")
+    print("        *                                       *")
+    print("        *            Escape Titanic             *")
+    print("        *                                       *")
+    print("        *****************************************")
 
-if answer == "left":
-    answer = input("You walked into a room and there’s an open window, you can either jump out of there hoping to get out alive or walk down the hall? Type jump to jump out of the window or walk to walk down the hall")
-
-    if answer == "jump":
-        print("You drowned and you died. Did you really think you could survive?")
-
-    elif answer == "walk":
-        answer = input("You arrive in front of a room either you walk back or you enter")
-        if answer == "walck back":
-            print()
-        elif answer == "enter":
-            answer = input("You see a drunken sailor who has a key ring, you either politely ask him for his keys or you knock him out and retrieve them")
-            if answer =="ask":
-                answer = input("You kindly ask him to give you his keys but he suddenly becomes aggressive, try to calm him down or you confront him")
-
-            elif answer == "Knock him out":
-                print("lol")
-            else:
-                print("Not a valid option")
-
-    else:
-        print("Not a valid option, you're dead")
-
-elif answer == "right":
-    print("")
-
-else:
-    print("Not a valid option, you're dead")
